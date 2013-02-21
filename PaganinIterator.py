1654edb5e5169efd181125667748445bd6c50a77
David Haberthür <email@davidhaberthuer.ch>
2013-02-21 14:09:19 +0100

    PaganinIterator now calls gridrec directly in a way so that the recs dont need to be moved to the correct directory

--- PaganinIterator.py
+++ PaganinIterator.py
@@ -20,40 +20,41 @@ os.system('clear')
 parser = OptionParser()
 usage = "usage: %prog [options] arg"
 parser.add_option('-D', '--Directory', dest='SampleFolder',
-	help='Location of the Sample you want to reconstruct with different parameters differently',
-	metavar='path')
-parser.add_option('-d', '--Delta', dest='Delta', type = 'float',
-	help='Delta you want to start with',
-	metavar='3e-10')
+                  help='Location of the Sample you want to reconstruct with different parameters differently',
+                  metavar='path')
+parser.add_option('-d', '--Delta', dest='Delta',
+                  type='float',
+                  help='Delta you want to start with',
+                  metavar='3e-10')
 parser.add_option('-b', '--Beta', dest='Beta', type='float',
-	help='Beta you want to start with',
-	metavar='3e-10')
+                  help='Beta you want to start with',
+                  metavar='3e-10')
 parser.add_option('-z', dest='Distance', default=50, type='int',
-	help='Distance to the scintillator in mm',
-	metavar=33)
+                  help='Distance to the scintillator in mm',
+                  metavar=33)
 parser.add_option('-r', '--range', dest='Range', type='int',
-	help='Range of powers you want to iterate through',
-	metavar='3')
+                  help='Range of powers you want to iterate through',
+                  metavar='3')
 parser.add_option('-c', '--center', dest='RotationCenter', type='float',
-    help='RotationCenter for reconstructing the slices (Default=Read from logfile or set to 1024 if nothing found in logfile)',
-    metavar='1280.5')
+                  help='RotationCenter for reconstructing the slices (Default=Read from logfile or set to 1024 if nothing found in logfile)',
+                  metavar='1280.5')
 parser.add_option('-v', '--verbose', dest='Verbose', default=0, action='store_true',
-	help='Be really chatty, (Default of the script is silent)',
-	metavar=1)
+                  help='Be really chatty, (Default of the script is silent)',
+                  metavar=1)
 parser.add_option('-t', '--test', dest='Test', default=0, action='store_true',
-	help='Only do a test-run to see the details, do not actually reconstruct the slices)',
-	metavar=1)
+                  help='Only do a test-run to see the details, do not actually reconstruct the slices)',
+                  metavar=1)
 (options, args) = parser.parse_args()
 
 # show the help if no parameters are given
 if options.SampleFolder is None:
-	parser.print_help()
-	print 'Example:'
-	print 'The command below calculates the Paganin reconstuctions of Sample A with Delta values varying from =3e-4 to 3e-10'
-	print ''
-	print sys.argv[0], '-D /sls/X02DA/data/e12740/Data10/disk1/Sample_A_ -d 3e-7 -r 3 -b 3e-10'
-	print ''
-	sys.exit(1)
+    parser.print_help()
+    print 'Example:'
+    print 'The command below calculates the Paganin reconstuctions of Sample A with Delta values varying from =3e-4 to 3e-10'
+    print ''
+    print sys.argv[0], '-D /sls/X02DA/data/e12740/Data10/disk1/Sample_A_ -d 3e-7 -r 3 -b 3e-10'
+    print ''
+    sys.exit(1)
 
 if options.Delta is None:
     print 'Your command was "' + ' '.join(sys.argv) + '"'
@@ -61,14 +62,14 @@ if options.Delta is None:
     print
     print 'please enter a Delta value with the -d parameter'
     sys.exit(1)
-	
+
 if options.Beta is None:
     print 'Your command was "' + ' '.join(sys.argv) + '"'
     print 'I cannot find a Beta value.'
     print
     print 'please enter a Beta value with the -b parameter'
     sys.exit(1)
-	
+
 if options.Range is None:
     print 'Your command was "' + ' '.join(sys.argv) + '"'
     print 'I cannot find a range to iterate.'
@@ -82,7 +83,6 @@ SampleName = os.path.basename(os.path.abspath(options.SampleFolder))
 # Get RotationCenter from Logfile, or set it to 1024 if not found.
 if options.RotationCenter is None:
     LogFileLocation = os.path.join(options.SampleFolder, 'tif', SampleName + '.log')
-    print LogFileLocation
     if options.Verbose:
         print 'Trying to get RotationCenter from', LogFileLocation
     LogFile = open(LogFileLocation, 'r')
@@ -104,42 +104,45 @@ if options.RotationCenter is None:
             print 'No Rotation center found in LogFile, setting it to 1024.'
 
 if options.Test:
-	print 'I would do this'
+    print 'I would do this'
 
-for i in range(-options.Range,options.Range+1):
+for i in range(-options.Range, options.Range+1):
     Delta = float(str('%e' % options.Delta)[:-2] + str(int(str('%e' % options.Delta)[-2:]) + i))
     # Construct Paganin-call
-    SinogramCommand = ' '.join(['~/scripts/sinooff_tomcat_paganin.py',os.path.abspath(os.path.join(options.SampleFolder,'tif')),str(Delta),str(options.Beta),str(options.Distance)])
-    ReconstructionCommand = 'gridrec' + ' -c ' + str(options.RotationCenter) + ' ' + os.path.abspath(os.path.join(options.SampleFolder,'sin'))
-    MoveRecCommand = ' '.join(['mv',os.path.abspath(os.path.join(options.SampleFolder,'sin','*rec*')),os.path.abspath(os.path.join(options.SampleFolder,'rec_' + str(Delta) + '_' + str(options.Beta)))])
-    MoveSinogramsCommand = ' '.join(['mv',os.path.abspath(os.path.join(options.SampleFolder,'sin')),os.path.abspath(os.path.join(options.SampleFolder,'sin_' + str(Delta) + '_' + str(options.Beta)))])
-    MoveFilteredProjectionsCommand = ' '.join(['mv',os.path.abspath(os.path.join(options.SampleFolder,'fltp')),os.path.abspath(os.path.join(options.SampleFolder,'fltp_' + str(Delta) + '_' + str(options.Beta)))])   
+    SinogramCommand = ' '.join(['~/scripts/sinooff_tomcat_paganin.py', os.path.abspath(os.path.join(options.SampleFolder, 'tif')), str(Delta), str(options.Beta), str(options.Distance)])
+    ReconstructionCommand = 'gridrec -f parzen -c ' + str(options.RotationCenter) + ' -D ' + os.path.abspath(os.path.join(options.SampleFolder, 'sin')) + ' -O ' + os.path.abspath(os.path.join(options.SampleFolder, 'rec_' + str(Delta) + '_' + str(options.Beta))) 
+    MoveSinogramsCommand = ' '.join(['mv', os.path.abspath(os.path.join(options.SampleFolder, 'sin')), os.path.abspath(os.path.join(options.SampleFolder, 'sin_' + str(Delta) + '_' + str(options.Beta)))])
+    MoveFilteredProjectionsCommand = ' '.join(['mv', os.path.abspath(os.path.join(options.SampleFolder, 'fltp')), os.path.abspath(os.path.join(options.SampleFolder, 'fltp_' + str(Delta) + '_' + str(options.Beta)))])
     if options.Test:
-        print SinogramCommand
-        print ReconstructionCommand
-        print MoveRecCommand
-        print MoveSinogramsCommand
-        print MoveFilteredProjectionsCommand
+        print 3 * ' ',SinogramCommand
+        print 3 * ' ',ReconstructionCommand
+        print 3 * ' ',MoveSinogramsCommand
+        print 3 * ' ',MoveFilteredProjectionsCommand
         print 80 * '_'
     else:
         print 'Generating sinograms'
         os.system(SinogramCommand)
-        print 'Reconstructing sinograms'
+        print 'Reconstructing sinograms into renamed folder'
         os.system(ReconstructionCommand)
-        print 'Moving reconstructions'
-        os.system(MoveRecCommand)
         print 'Renaming sinogram folder'
         os.system(MoveSinogramsCommand)
         print 'Renaming filtered projections folder'
         os.system(MoveFilteredProjectionsCommand)
 
 if options.Test:
-	print 'I was just testing'
+    print 'I was just testing'
 else:
-	print 'Done with what you have asked for.'
-	print
-	print 'You now have the Sinogram-Directories'
-	for i in range(-options.Range,options.Range+1):
-		Delta = float(str('%e' % options.Delta)[:-2] + str(int(str('%e' % options.Delta)[-2:]) + i))
-		print '    *',os.path.abspath(os.path.join(options.SampleFolder,'sin_' + str(Delta) + '_' + str(options.Beta)))
-	print 'Use ReconstructSinogram.py for each of those to actually recostruct a slice'
+    print 'Done with what you have asked for.'
+    print
+    print 'You now have the sinogram directories'
+    for i in range(-options.Range, options.Range+1):
+        Delta = float(str('%e' % options.Delta)[:-2] + str(int(str('%e' % options.Delta)[-2:]) + i))
+        print '    *', os.path.abspath(os.path.join(options.SampleFolder, 'sin_' + str(Delta) + '_' + str(options.Beta)))
+    print 'the filtered projection directories'
+    for i in range(-options.Range, options.Range+1):
+        Delta = float(str('%e' % options.Delta)[:-2] + str(int(str('%e' % options.Delta)[-2:]) + i))
+        print '    *', os.path.abspath(os.path.join(options.SampleFolder, 'fltp_' + str(Delta) + '_' + str(options.Beta)))        
+    print 'and the reconstruction directories'
+    for i in range(-options.Range, options.Range+1):
+        Delta = float(str('%e' % options.Delta)[:-2] + str(int(str('%e' % options.Delta)[-2:]) + i))
+        print '    *', os.path.abspath(os.path.join(options.SampleFolder, 'rec_' + str(Delta) + '_' + str(options.Beta)))        
