b42d558cae5a32711dd35ddca5dd0518c580f378
Kevin Mader <kevin.mader@gmail.com>
2013-12-08 20:18:30 +0100

    Updated script to create multiple scans

--- postscan/multiscanViewer.py
+++ postscan/multiscanViewer.py
@@ -3,21 +3,17 @@ from PIL import Image
 import os,sys
 import numpy
 sOffset=0
-angOffset=180.01
+angOffset=180-0.001 # (1.0/15000001.0*180.0)
 
 if len(sys.argv)<2: sliceNumber=1000
 else: sliceNumber=int(sys.argv[1])
-
+sampleName=os.path.abspath('.').split('/')[-1]
 
 def processImage(inFile,outFile,scanNumber):
 	cImg=Image.open(inFile)
-	cImg=cImg.rotate(scanNumber*angOffset+sOffset)# ,filter=Image.BILINEAR,expand=0)
-	#pix=numpy.array(cImg.getdata()).reshape(cImg.size[0],cImg.size[1])
-	
-	#if(scanNumber%2==1): print ('flipping',scanNumber) #pix=numpy.flipud(pix) #numpy.rot90(pix,2)
-	#cImg.putdata(list(tuple(pixel) for pixel in pix))
+	cImg=cImg.rotate(scanNumber*angOffset+sOffset)
 	cImg.save(outFile)
-	#return pix
+
 def generate_slices(sliceNumber):
 	outDirName="slicedir_%04d" % (sliceNumber)
 	imglist=glob("rec*/*%04d*.tif" % (sliceNumber))
@@ -28,7 +24,9 @@ def generate_slices(sliceNumber):
 	imgdict=dict(zip(scanNumber,imglist))
 	
 	if (not os.path.exists(outDirName)): os.makedirs(outDirName)
-	map(lambda i: processImage(imgdict[i],outDirName+"/%03d.tif" % (i),i),sorted(imgdict.keys()))
+	outNameFn=lambda i: outDirName+"/%s.%04d.%03d.tif" % (sampleName,sliceNumber,i)
+	map(lambda i: processImage(imgdict[i],outNameFn(i),i),sorted(imgdict.keys()))
+	print ('Slice %04d with offset %02.2f generated for scans:' % (sliceNumber,angOffset),imgdict.keys())
 
 if sliceNumber>=0:
 	generate_slices(sliceNumber)
