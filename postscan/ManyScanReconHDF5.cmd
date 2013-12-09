97777d1df4e9eb92bd54305a712195360a950637
Kevin Mader <kevinmader@gmail.com>
2013-12-09 17:49:39 +0100

    Fixed a few bugs in julies script to it stops free run after the scan, remodulos the stage, and sets the speed back to the default. Also expanded the manyscanrecon script to crop the image and include information on holding/releasing jobs

--- postscan/ManyScanReconHDF5.cmd
+++ postscan/ManyScanReconHDF5.cmd
@@ -1,14 +1,40 @@
-# run from 1 to 10
-# for i in $( seq 1 10 ); do ../makehdf5.cmd $i; done
-# kill all reconstruction jobs qdel -u r14574
+
+## run just job number 1 (for single scans)
+# ~/beamline-scripts/postscan/ManyScanReconHDF5.cmd 1
+## run scans from 1 to 10
+# for i in $( seq 1 10 ); do  ~/beamline-scripts/postscan/ManyScanReconHDF5.cmd $i; done
+## list all reconstruction jobs running: qstat
+# qstat -xml | grep JB_name
+## kill a single job 
+# qdel (job-iD from qstat)
+## kill all reconstruction jobs 
+# qdel -u e14574
+## prevent jobs (hold)  486385 to 486711 from running
+# for i in `seq 486385 486711`; do qhold $i; done
+## prevent all jobs for user e14574 in the queue currently from running
+# qhold -u e14574
+## release jobs to run 
+# for i in `seq 486385 486711`; do qrls $i; done
+## tell all jobs of user e14574 not to send mail
+# qalter -m n -u e14574
+## change the address for all jobs of user e14574
+# qalter -M kevinmader+tomcatsge@gmail.com -u e14574
+
 
 # important settings
 darkCnt=20
 flatCnt=80
 projCnt=1501
-reconSettings="-c 1280 -n -0.002 -x 0.003 -U 0.3 -t 16 -F parz -Z 0.5" 
+# center, minimum, maximum value, parzen filter setting, 16 bit, filter, zero padding, cut projections (left, right, top, bottom)
+reconSettings="-c 1280 -n -0.002 -x 0.003 -U 0.3 -t 16 -F parz -Z 0.5 -r 0,0,720,0" # 
+ncores=12 # number of cores to use (if not cutting, full ROI, it should be 30, if cutting 700+ slices then 12)
 # zinger = -z, zinger width = L
-
+# wavelet -y db20 (db15-db20)
+# -V 1-5 (larger number for larger rings)
+# -E 2.0 (larger for larger rings)
+# region of interest in image -e 1,720,2560,2160 x from 1 to 2560, y from 720 to 2160
+# email to send job status to
+email="kevinmader+tomcatsge@gmail.com"
 # load libraries and execute 
 
 module use /opt/xbl-software/modulefiles-private
@@ -19,10 +45,6 @@ module use /opt/xbl-software/modulefiles
 module load xbl/mpi
 module load xbl/epd
 module load xbl/tifffile/2012.01.01
-
-
-
-
 
 # name of the current directory is assumed to be the sample name
 preFix=${PWD##*/}
@@ -66,7 +88,7 @@ prjLine="/afs/psi/project/TOMCAT_pipeline/Beamline/tomcat_pipeline/src/prj2sinSG
 sflatCnt=$((flatCnt/2))
 sdarkCnt=$((darkCnt))
 prjSettings="-f $projCnt,$sdarkCnt,$sflatCnt,0,0"
-stdSge="$prjLine --ncores=60 --email=kevinmader+tomcatsge@gmail.com -R $scanNumber -I 2 -d"
+stdSge="$prjLine --ncores=$ncores --email=$email --jobname=s${scanNumber}_${preFix} -R $scanNumber -I 2 -d"
 dirSettings="-o $cDir/sino_${scanNumber}/ $hdfName"
 prjCmd="$stdSge $prjSettings $reconSettings $dirSettings"
 $prjCmd
