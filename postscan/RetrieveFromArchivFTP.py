9f02af416c0efe2fa755cbf202334d4307653dd2
David Haberthür <email@davidhaberthuer.ch>
2014-05-08 12:01:23 +0200

    Do not use shell=TRUE
    And make some details a bit spiffier in the script.

--- postscan/RetrieveFromArchivFTP.py
+++ postscan/RetrieveFromArchivFTP.py
@@ -8,6 +8,7 @@ from optparse import OptionParser
 import getpass
 import os
 import subprocess
+import commands
 
 # Setup
 MountPoint = '/mnt/archiv/project/sls'
@@ -17,7 +18,7 @@ parser = OptionParser()
 usage = "usage: %prog [options] arg"
 parser.add_option('-s', '--Sample', dest='Sample', type='str',
                   help='(incomplete) Name of the sample you would like to '
-                  'retrieve from archivftp.psi.ch',
+                  'retrieve from archivftp.psi.ch. *No* wildcard needed!',
                   metavar='R108C60')
 parser.add_option('-e', '--eAccount', dest='eAccount', type='str',
                   help='eAccount in which I should look for (defaults to the '
@@ -29,11 +30,11 @@ parser.add_option('-e', '--eAccount', dest='eAccount', type='str',
 if options.Sample is None:
     parser.print_help()
     print 'Example:'
-    print 'The command below shows looks for all samples R108C60* for the',\
-        'eAccount e11126 on the archive and downloads them to subdirectories',\
-        'of the current directory.'
+    print 'The command below shows looks for all samples for which the name',\
+        'contains C60 for the eAccount e11126 on the archive and downloads',\
+        'them to subdirectories of the current directory.'
     print ''
-    print sys.argv[0], '-e e11126 -s R108C60'
+    print sys.argv[0], '-e e11126 -s C60'
     print ''
     sys.exit(1)
 
@@ -41,7 +42,7 @@ if options.eAccount is None:
     options.eAccount = getpass.getuser()
 
 # Go!
-print 80 * '_'
+print 75 * '_'
 
 print 'We are looking for sample "' + options.Sample + '*" on x02da-cn-6.'
 print
@@ -49,23 +50,23 @@ print 'For this I will start to look for files on',\
     os.path.join(MountPoint, options.eAccount), '(and ask you for the',\
     'password of the eAccount).'
 
-
 # Generate command to list the files on the MountPoint on x02da-cn-6
 # From http://is.gd/ZVlIg9
-sshCommand = 'ssh -q', options.eAccount + '@x02da-cn-6 \'find',\
-    os.path.join(MountPoint, options.eAccount), '-name', options.Sample + '*\''
-sshCommand = ' '.join(sshCommand)
+sshCommand = 'ssh', '-q', options.eAccount + '@x02da-cn-6', 'find',\
+    os.path.join(MountPoint, options.eAccount), '-name',  '\'*' +\
+    options.Sample + '*\''
 
 print
 print 'I will use the command below to find what you are looking for'
+print 'Until then: http://youtu.be/gY75dw64sqI'
 print '---'
-print sshCommand
+print ' '.join(sshCommand)
 print '---'
 
-sshProcess = subprocess.Popen(sshCommand, stdout=subprocess.PIPE, shell=True)
+sshProcess = subprocess.Popen(sshCommand, stdout=subprocess.PIPE)
 SampleList = []
-# Save all found samples in a list, so we can use them lateron to present
-# to the user: http://stackoverflow.com/a/2813530/323100
+# Save all found samples in a list, so we can use them to present to the user
+# http://stackoverflow.com/a/2813530/323100
 while True:
     line = sshProcess.stdout.readline()
     if line.startswith(MountPoint):
@@ -73,15 +74,14 @@ while True:
     else:
         break
 
-print 80 * '_'
+print 75 * '_'
 
-print 'In', os.path.commonprefix(SampleList), 'I found:'
+print 'In', os.path.commonprefix(SampleList) + '* I found:'
 Counter = 0
 for Sample in SampleList:
     # http://stackoverflow.com/a/11784589/323100
     print '    [' + str(Counter) + ']\033[1m',  os.path.basename(Sample),\
-        '\033[0min',\
-        os.path.dirname(Sample)[len(os.path.commonprefix(SampleList)):]
+        '\033[0min', os.path.dirname(Sample)
     Counter += 1
 
 print 'Which sample should I get back from archivFTP?'
@@ -89,7 +89,7 @@ SampleNumberToGet = int(raw_input('Enter the sample number (from 0 to ' +
                                   str(Counter - 1) + '):'))
 SampleToGet = SampleList[SampleNumberToGet]
 
-print 80 * '_'
+print 75 * '_'
 
 if not os.path.exists(os.path.basename(SampleToGet)):
     os.mkdir(os.path.basename(SampleToGet))
@@ -98,22 +98,27 @@ print 'Saving archivftp:' +\
     SampleToGet[len(os.path.commonprefix(SampleList)):], '/tif/TARCHIVE/'\
     'tif.tar to', os.path.abspath(SampleToGet), '/tif.tar'
 
-rsyncCommand = 'rsync -arvhP', options.eAccount + '@x02da-cn-6:' +\
+rsyncCommand = 'rsync', '-arvhP', options.eAccount + '@x02da-cn-6:' +\
     SampleToGet + '/tif/TARCHIVE/tif.tar',\
     os.path.abspath(os.path.basename(SampleToGet))
-rsyncCommand = ' '.join(rsyncCommand)
 
 print
 print 'I will use the command below to actually get back the tif.tar'
 print '---'
-print rsyncCommand
+print ' '.join(rsyncCommand)
 print '---'
 print 'This will take a (long) while'
-subprocess.Popen(rsyncCommand, stdout=subprocess.PIPE, shell=True)
+subprocess.call(rsyncCommand)
 
-print 80 * '_'
+print 75 * '_'
 
-print 'Now we can unpack', os.path.abspath(os.path.basename(SampleToGet)) +\
-    '/tif.tar to ./' + os.path.basename(SampleToGet) + '/tif/*'
-os.chdir(os.path.abspath(os.path.basename(SampleToGet)))
-subprocess.Popen('tar -xvf tif.tar', stdout=subprocess.PIPE, shell=True)
+unpackCommand = 'tar', '-xvf',\
+    os.path.join(os.path.abspath(os.path.basename(SampleToGet)), 'tif.tar')
+print 'Now we unpack', os.path.abspath(os.path.basename(SampleToGet)) +\
+    '/tif.tar to ./' + os.path.basename(SampleToGet) + '/tif/* by calling',\
+    ' '.join(unpackCommand)
+
+subprocess.call(unpackCommand)
+
+print 75 * '_'
+print 'All done for sample', os.path.basename(SampleToGet)
