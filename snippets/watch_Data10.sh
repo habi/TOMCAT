9d32be92440924ecb300df2e41321e6336a2940a
David Haberthür <email@davidhaberthuer.ch>
2014-12-16 16:58:30 +0100

    Documentation effort
    Give a tiny bit of information in the header of the .sh files

--- snippets/watch_Data10.sh
+++ snippets/watch_Data10.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# watch_Data10.sh | David Haberthür <david.haberthuer@psi.ch>
+# Small bash snippet to view how much space is used on Data10.
+# By "used" we mean only the stuff synched to the users HDs.
 watch -n 1800 "echo ---;du ~/Data10/disk* -sh --exclude-from ~/exclude.txt"
--- snippets/watch_ExternalDisks.sh
+++ snippets/watch_ExternalDisks.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# watch_ExternalDisks.sh | David Haberthür <david.haberthuer@psi.ch>
+# Small bash snippet to view how much data is already synchronized to the disks.
+# Should be run on the media station once the users HDs are conneted and syncing.
 watch -n 1800 "df -h /dev/sd?1"
