816e63fe4268ca541bea56491190b9b9636c18e5
David Haberthuer <david.haberthuer@psi.ch>
2015-04-14 12:45:11 +0200

    Run watching command every ten minutes instead of every 30

--- snippets/watch_ExternalDisks.sh
+++ snippets/watch_ExternalDisks.sh
@@ -2,4 +2,4 @@
 # watch_ExternalDisks.sh | David Haberthür <david.haberthuer@psi.ch>
 # Small bash snippet to view how much data is already synchronized to the disks.
 # Should be run on the media station once the users HDs are conneted and syncing.
-watch -n 1800 "df -h /dev/sd?1"
+watch -n 600 "df -h /dev/sd?1"
