30b2f83f566b29a716360bf4ffe7b015e84ab381
David Haberthür <email@davidhaberthuer.ch>
2015-04-24 13:03:55 +0200

    Reading DMP files and showing three slices each
    The script now also reads DMP files (we are importing the dmp-files
    repository as submodule)
    The script now displays three images for each sample, one at 1/4, 1/2
    and 3/4 of the height of the stack.

--- snippets/ViewReconstructions.py
+++ snippets/ViewReconstructions.py
@@ -21,35 +21,92 @@ import numpy
 import matplotlib.pyplot as plt
 import glob
 import os
+import imp
+dmpreader = imp.load_source('HandleDMP',
+                            os.path.join(os.path.expanduser('~'),
+                                         'beamline-scripts/postscan/'
+                                         'dmp-files/Python/HandleDMPFiles.py'))
 try:
     from tifffile import tifffile
 except ImportError:
-    print 'To use this script, you need to load two modules'
-    print 'Use the commands below to do this:'
-    print '   module load xbl/epd'
-    print '   module load xbl/tifffile/2012.01.01'
+    print 'To use this script, we need to load two modules, the Enthought ' \
+          'Python Distribution and the tiff file library.'
+    print 'To do so, please enter the command below into the terminal and ' \
+          'start the script again.'
+    print 'module load xbl/epd; module load xbl/tifffile/2012.01.01'
     exit()
 
+# Detect all subfolders
+print 'Looking for reconstructions in all subfolders of %s' % \
+      os.path.abspath(os.getcwd())
 CurrentDisk = os.path.basename(os.getcwd())
-Slice = 1280
-print 'Looking for reconstructions in all subfolders of %s' % CurrentDisk
+ReconstructionFolders = glob.glob(os.path.join('/sls/X02DA/Data10/e15423',
+                                               CurrentDisk, '*', 'rec_*'))
+if not ReconstructionFolders:
+    print 'No reconstruction subfolders found.'
+    print 'Copy the script to ~/Data10/diskX and start it from there'
+    exit()
 
-Reconstructions = glob.glob(os.path.join('/sls/X02DA/Data10/e15423',
-                                         CurrentDisk, '*', 'rec_*',
-                                         '*' + str(Slice) + '*'))
+print 'Showing three slices for each of the %s found reconstruction ' \
+      'folders in the subfolder of %s\n' \
+      % (len(ReconstructionFolders), CurrentDisk)
 
-print 'Showing slice %s of each rec_* folder' % Slice
+for counter, CurrentFolder in enumerate(ReconstructionFolders):
+    if len(glob.glob(os.path.join(CurrentFolder, '*.DMP'))):
+        Suffix = 'DMP'
+    elif len(glob.glob(os.path.join(CurrentFolder, '*8bit*'))):
+        Suffix = '8bit.tif'
+    elif len(glob.glob(os.path.join(CurrentFolder, '*16bit*'))):
+        Suffix = '16bit.tif'
+    else:
+        print 'No image files found in %s' % CurrentFolder
+        print 'Maybe this sample is still pending on the cluster...\n'
+        Suffix = []
 
-for counter, FileName in enumerate(Reconstructions):
-    print 'Reconstruction %s of %s: %s' % (counter + 1,
-                                           len(Reconstructions),
-                                           os.path.basename(FileName))
-    with tifffile(FileName) as TIFFFile:
-        Image = TIFFFile.asarray()
-    plt.figure(figsize=[12, 12])
-    plt.imshow(Image, cmap='gray', interpolation='none')
-    plt.title(str(counter + 1) + '/' + str(len(Reconstructions)) + ': ' +\
-              os.path.basename(FileName) + ' from ' +
-              os.path.dirname(FileName))
-    plt.tight_layout()
-    plt.show()
+    if Suffix:
+        print 'Reading three %s files from the %s ones found in %s\n' % (
+            Suffix, len(glob.glob(os.path.join(CurrentFolder, '*' + Suffix))),
+            CurrentFolder)
+        FileTop = sorted(glob.glob(os.path.join(CurrentFolder,
+                                                '*.' + Suffix)))[
+            len(glob.glob(os.path.join(CurrentFolder, '*.' + Suffix)))/4]
+        FileMid = sorted(glob.glob(os.path.join(CurrentFolder,
+                                                '*.' + Suffix)))[
+            2 * len(glob.glob(os.path.join(CurrentFolder, '*.' + Suffix))) / 4]
+        FileBot = sorted(glob.glob(os.path.join(CurrentFolder,
+                                                '*.' + Suffix)))[
+            3 * len(glob.glob(os.path.join(CurrentFolder, '*.' + Suffix))) / 4]
+        if Suffix == 'DMP':
+            ImageTop = dmpreader.readDMP(FileTop)
+            ImageMid = dmpreader.readDMP(FileMid)
+            ImageBot = dmpreader.readDMP(FileBot)
+        else:
+            with tifffile(FileTop) as TIFFFile:
+                ImageTop = TIFFFile.asarray()
+            with tifffile(FileMid) as TIFFFile:
+                ImageMid = TIFFFile.asarray()
+            with tifffile(FileBot) as TIFFFile:
+                ImageBot = TIFFFile.asarray()
+        plt.figure(figsize=[20, 9])
+        plt.subplot(131)
+        plt.imshow(ImageTop, cmap='gray', interpolation='none')
+        plt.title('Slice %s' %
+                  str(len(glob.glob(os.path.join(CurrentFolder,
+                                                 '*.' + Suffix)))/4+1))
+        plt.subplot(132)
+        plt.imshow(ImageMid, cmap='gray', interpolation='none')
+        plt.title('Slice %s' %
+                  str(2*len(glob.glob(os.path.join(CurrentFolder,
+                                                   '*.' + Suffix)))/4+1))
+        plt.subplot(133)
+        plt.imshow(ImageBot, cmap='gray', interpolation='none')
+        plt.title('Slice %s' %
+                  str(3*len(glob.glob(os.path.join(CurrentFolder,
+                                                   '*.' + Suffix)))/4+1))
+        plt.suptitle('%s/%s: three slices of %s from %s' % (
+            counter + 1, len(ReconstructionFolders),
+            len(glob.glob(os.path.join(CurrentFolder, '*.' + Suffix))),
+            CurrentFolder))
+        plt.tight_layout()
+        plt.show()
+print 'Done'
